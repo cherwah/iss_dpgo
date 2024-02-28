1bed11536add79bc40d27880eaf42328ff08b3e4
Tan Cher Wah <cherwah@gmail.com>
2024-02-28 20:22:09 +0800

    .

--- code_dpgo/abstract_factory/main.go
+++ code_dpgo/abstract_factory/main.go
@@ -10,11 +10,13 @@ func main() {
 	os := runtime.GOOS
 
   var factory factory.Factory = factory.Get_factory(os)
-  if factory != nil {
-    factory.Make_Button()
-    factory.Make_Checkbox()
-    factory.Make_Dropdown()
-  } else {
+  if factory == nil {
     fmt.Println("Unsupported OS.")
+    return
   }
+
+  // create OS-specific widgets
+  factory.Make_Button()
+  factory.Make_Checkbox()
+  factory.Make_Dropdown()
 }
